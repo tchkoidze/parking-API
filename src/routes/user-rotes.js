6077345e9f20751e980eab42c8451d403b6b086b
tchkoidze <ilia.chkoidze12@gmail.com>
2023-09-23 17:37:45 +0400

    chore

--- src/routes/user-rotes.js
+++ src/routes/user-rotes.js
@@ -4,6 +4,6 @@ import { getAllUsers, signup } from "../controllers/user-controllers.js";
 const userRouter = express.Router();
 
 userRouter.post("/signup", signup);
-productRouter.get("/users", getAllUsers);
+userRouter.get("/users", getAllUsers);
 
 export default userRouter;
