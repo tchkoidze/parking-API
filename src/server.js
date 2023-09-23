d99d3aefd1d4e9100a3badfa2c9e450372f6b1fb
tchkoidze <ilia.chkoidze12@gmail.com>
2023-09-23 14:25:58 +0400

    feat: add get request for test

--- src/server.js
+++ src/server.js
@@ -1,3 +1,22 @@
-import express from "express";
+import express, { request, response } from "express";
+import { createUserTable } from "./config/sql.js";
 
 const app = express();
+
+async function init() {
+  try {
+    await createUserTable();
+    serverStart();
+  } catch (error) {
+    console.log(error);
+  }
+
+  function serverStart() {
+    app.get("/", (request, response) => {
+      return response.status(200).json({ message: "works!" });
+    });
+    app.listen(3000);
+  }
+}
+
+init();
