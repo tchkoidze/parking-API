570c791676033b585e86f9ea9670accb8724fb97
tchkoidze <ilia.chkoidze12@gmail.com>
2023-09-24 18:43:09 +0400

    chore

--- src/Schemas/user-registration-schema.js
+++ src/Schemas/user-registration-schema.js
@@ -20,6 +20,17 @@ const userRegistrationSchema = async () => {
         "string.email": "email should be a valid email address",
         "any.required": "email is required",
       }),
+    passwords: Joi.string()
+      .min(8)
+      .pattern(/^(?=.*[A-Z])(?=.*[a-zA-Z0-9!,-_.]).*$/)
+      .required()
+      .messages({
+        "string.base": "password should be a string",
+        "string.min": "password should consist of minimum 8 characters",
+        "string.pattern":
+          "password must start with a capital letter and can contain letters, digits, and characters like !, -, _, and .",
+        "string.required": "password isrequired",
+      }),
   });
 };
 
