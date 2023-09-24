1be979b1e07915baeeef4e0fd1e5321ab522a71e
tchkoidze <ilia.chkoidze12@gmail.com>
2023-09-24 17:22:48 +0400

    chore

--- src/Schemas/user-registration-schema.js
+++ src/Schemas/user-registration-schema.js
@@ -27,6 +27,8 @@ const userRegistrationSchema = async () => {
       .messages({
         "string.base": "password should be a string",
         "string.min": "password should consist of minimum 8 characters",
+        "string.pattern":
+          "password must start with a capital letter and can contain letters, digits, and characters like !, -, _, and .",
         "string.required": "password isrequired",
       }),
   });
