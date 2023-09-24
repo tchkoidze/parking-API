6a2553d9df628e45fd151ddbb43fb3e2832dbd0d
tchkoidze <ilia.chkoidze12@gmail.com>
2023-09-24 18:50:27 +0400

    choe

--- src/config/sql.js
+++ src/config/sql.js
@@ -2,11 +2,15 @@ import pgk from "pg";
 const { Pool } = pgk;
 
 const pool = new Pool({
-  host: "dpg-ck7ai4fsasqs73a1rkr0-a",
+  //host: "dpg-ck7ai4fsasqs73a1rkr0-a",
+  host: "dpg-ck84njvsasqs73bp3pgg-a",
   port: 5432,
-  database: "parking_tz9v",
-  user: "parking_tz9v_user",
-  password: "KiQ0S15Wfmw789wWVdsC5yuLI0sTn31Z",
+  //database: "parking_tz9v",
+  database: "parking_sql",
+  //user: "parking_tz9v_user",
+  user: "parking_sql_user",
+  //password: "KiQ0S15Wfmw789wWVdsC5yuLI0sTn31Z",
+  password: "JcshoqBKlvyyovLGVEOp5LSkrpf48eXg",
 });
 
 export const createUserTable = async () => {
