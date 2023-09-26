e20cd1b452a2cfd2ab939522d7a92d29adc2a9b7
tchkoidze <ilia.chkoidze12@gmail.com>
2023-09-26 12:46:57 +0400

    chore

--- src/controllers/user-controllers.js
+++ src/controllers/user-controllers.js
@@ -106,7 +106,7 @@ export const login = async (req, res) => {
     console.log("isMatch:", isMatch);
 
     if (isMatch) {
-      const token = jwt.sign(user.rows[0].email, process.env.JWT_SECRET);
+      //const token = jwt.sign(user.rows[0].email, process.env.JWT_SECRET);
       console.log("data is correct:", body.email, body.password);
 
       return res.status(200).json({ message: "Login successful!", token });
