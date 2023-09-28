cc7a084634bffac7562553ebab89906208f75420
tchkoidze <ilia.chkoidze12@gmail.com>
2023-09-28 11:54:48 +0400

    chore: commented snipet at auth-middlewares

--- src/middlewares/auth-middleware.js
+++ src/middlewares/auth-middleware.js
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from "express";
+//import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
 const authMiddleware = (req, res, next) => {
