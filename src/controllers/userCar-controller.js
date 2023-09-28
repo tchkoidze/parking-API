84d01c1da4fdfe7e15138e8945c2b200b1e86d23
tchkoidze <ilia.chkoidze12@gmail.com>
2023-09-28 13:34:14 +0400

    feat: create updateCar request

--- src/controllers/userCar-controller.js
+++ src/controllers/userCar-controller.js
@@ -20,7 +20,7 @@ export const addCar = async (req, res) => {
     }
 
     await pool.query(
-      "INSERT INTO usercar(user_id, car_name, registration_plate, type) VALUES($1, $2, $3, $4) ",
+      "INSERT INTO usercar(userId, carName, registrationPlate, type) VALUES($1, $2, $3, $4) ",
       [paramsUserId, carName, registrationPlate, type]
     );
 
@@ -30,4 +30,32 @@ export const addCar = async (req, res) => {
   }
 };
 
-export const updateCar = async (req, res) => {};
+export const updateCar = async (req, res) => {
+  //const { body } = req;
+  const { carName, registrationPlate, type } = req.body;
+  const id = req.params.userId;
+  try {
+    const user = await pool.query("SELECT * FROM users WHERE id = $1 ", [
+      paramsUserId,
+    ]);
+
+    if (user.rows.length === 0) {
+      return res.status(401).json({ message: "User can't be identified!" });
+    }
+
+    const car = await pool.query("SELECT * FROM usercar WHERE userId = $1 ", [
+      id,
+    ]);
+    if (car.rows.length === 0) {
+      return res.status(400).json({ message: "Car did'not find" });
+    }
+    await pool.query(
+      "UPDATE usercar SET  carName=$1, registrationPlate=$2, type=$3 WHERE userId = $4",
+      [carName, registrationPlate, type, id]
+    );
+
+    return res.status(204).json({ message: "Car updated" });
+  } catch (error) {
+    return res.status(401).json(error);
+  }
+};
--- src/routes/userCar-router.js
+++ src/routes/userCar-router.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { addCar } from "../controllers/userCar-controller.js";
+import { addCar, updateCar } from "../controllers/userCar-controller.js";
 import authMiddleware from "../middlewares/auth-middleware.js";
 
 const userCarRouter = express.Router();
 
 userCarRouter.post("/usercar/:userId", authMiddleware, addCar);
+userCarRouter.put("/usercar/:userId", authMiddleware, updateCar);
 
 export default userCarRouter;
